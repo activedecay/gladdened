7961b4f944065e13efe4c6783dcd057289e32525
sloph <activedecay@gmail.com>
2021-11-12 17:51:38 -0700

    checker texture is unused but exists i guess

--- main.c
+++ main.c
@@ -4,19 +4,25 @@
 enum VAO_IDs {
   Triangles, NumVAOs
 };
+GLuint VAOs[NumVAOs];
 enum Buffer_IDs {
   VertexBuffer, NumBuffers
 };
-GLuint VAOs[NumVAOs];
+GLuint Buffers[NumBuffers];
 
-/* texture objects */
-enum TO_IDs {
-  Fonts, NumTOs
+
+/* glCreateTextures glDeleteTextures @p.260 */
+enum TextureObjectIds {
+  Fonts, NumTextureObjects
 };
+/* glCreateTextures glDeleteTextures @p.260 */
+GLuint TextureObjects[NumTextureObjects];
+/* glTextureStorageXD @p.261 */
 enum Texture_IDs {
   FontBuffer, NumTextures
 };
-GLuint TOs[NumTOs];
+/* glTextureStorageXD @p.261 */
+GLuint Textures[NumTextures];
 
 enum Attrib_IDs {
   vPosition = 0
@@ -31,8 +37,6 @@ enum Attrib_IDs {
  *  or if these two calls are chained together
  *  glBindBuffer glBufferStorage
  */
-GLuint Buffers[NumBuffers];
-GLuint Textures[NumTextures];
 
 
 int main() {
@@ -65,6 +69,15 @@ int main() {
 
 
 
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
 /* todo an attempt was made to create a texture
 
@@ -81,10 +94,37 @@ int main() {
   glNamedBufferStorage(Textures[FontBuffer], textureSize, texture, GL_DYNAMIC_STORAGE_BIT);
 */
 
+  /* access with shader variable sampler2D @p.259 */
+  glCreateTextures(GL_TEXTURE_2D, NumTextureObjects, TextureObjects); // todo glDeleteTextures()
+  GLuint texture_name = TextureObjects[Fonts];
+  GLsizei mip_levels = 0;
+  int texture_width = 8;
+  int texture_height = 8;
+  glTextureStorage2D(texture_name, mip_levels, GL_RGBA32F, texture_width, texture_height);
+  GLfloat texture_checkerboard[] = {
+    1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f,
+    1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f,
+    1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f,
+    1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f,
+    1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f,
+    1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f,
+    1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f,
+    1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f, 1.f,1.f,1.f,1.f, 0.f,0.f,0.f,1.f,
+  };
+  /* this is static data @p.268. we'll do buffer reads from memory to textures in a moment. */
+  glTextureSubImage2D(texture_name, 0, 0, 0, texture_width, texture_height, GL_RGBA, GL_FLOAT, texture_checkerboard);
+  /* unit must be between 0 and GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS - 1 */
+  glBindTextureUnit(0, texture_name);
 
 
-
-
+  
+  
+  
+  
+  
+  
+  
+  
 
   glGenVertexArrays(NumVAOs, VAOs);
   glBindVertexArray(VAOs[Triangles]);
